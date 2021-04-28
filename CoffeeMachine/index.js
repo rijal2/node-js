990d074fe7eb132a0214a3f030858859e882791a
rijal2 <merentas5benua27@gmail.com>
2021-04-28 22:14:06 +0700

    Menambah kan kede dari praktik Multiple export value in Node.js

--- CoffeeMachine/index.js
+++ CoffeeMachine/index.js
@@ -5,3 +5,21 @@ console.log("Mencampurkan air dan kopi");
 console.log("Menuangkan kopi ke dalam gelas");
 console.log("Menuangkan susu ke dalam gelas");
 console.log("Kopi Anda sudah siap!");
+
+//cara mengimport module yang sudah diexport oleh state.js
+const coffeeStock = require('./state');
+ 
+console.log(coffeeStock);
+
+//Contoh mengimport module dan diaplikasikan
+const coffeeStock = require('./state');
+ 
+const makeCoffee = (type, miligrams) => {
+    if (coffeeStock[type] >= miligrams) {
+        console.log("Kopi berhasil dibuat!");
+    } else {
+        console.log("Biji kopi habis!");
+    }
+}
+ 
+makeCoffee("robusta", 80);
